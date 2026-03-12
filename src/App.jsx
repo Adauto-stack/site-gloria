f7f590b0452d579d091424bd503eecd79c6564e3
Adauto Neto <joseadautoalves51@gmail.com>
2026-03-12 00:32:26 -0300

    Adicionando fotos e musicas

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import './App.css'
-import TelaInicial from './componentes/telainicial.jsx'; 
-import FloatingHearts from './componentes/floatinghearts.jsx';
-import ReactDOM from 'react-dom/client'
-import Tela2 from './componentes/tela2.jsx';
-import { Routes, Route } from 'react-router-dom';
+import TelaInicial from './componentes/telainicial.jsx'
+import FloatingHearts from './componentes/floatinghearts.jsx'
+import Tela2 from './componentes/tela2.jsx'
+// Importe apenas Routes e Route aqui
+import { Routes, Route } from 'react-router-dom'
+
 function App(){
   return(
-    <div>
+    <>
+      {/* Os corações ficam fora das Routes para aparecerem em tudo */}
       <FloatingHearts />
- 
+
       <Routes>
         <Route path="/" element={<TelaInicial />} />
         <Route path="/tela2" element={<Tela2 />} />
       </Routes>
-
-      
-
-    </div>
+    </>
   )
 }
-export default App;
+
+export default App
